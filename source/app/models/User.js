2ff8130a48fd332ccf451024a4b62ef5d034ec34
faisalsaeed-work <87143179+faisalsaeed-work@users.noreply.github.com>
2022-04-10 00:01:29 +0500

    Add encryption to password

--- source/app/models/User.js
+++ source/app/models/User.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcrypt')
 const db = require('./../../config/db');
 const {getDate, camelToSnakeCase} = require('./../services/HelperService')
 const table = 'users';
+const saltRounds = 10;
 
 const get = async (where = null) => {
     let res;
@@ -19,6 +21,9 @@ const findByEmail = async (email) => {
 
 const create = async (data) => {
     data = {...data, created_at:getDate(), updated_at:getDate()}
+    if(data.password){
+        data.password = bcrypt.hashSync(data.password, saltRounds);
+    }
     const keys = Object.keys(data)
     const values = keys.map((key, i)=>'$'+(i+1)).join(', ')
     let res = await db.query(`INSERT INTO ${table} (${keys.join(', ')}) VALUES (${values}) RETURNING *`, keys.map(key=>data[key]))
@@ -26,11 +31,19 @@ const create = async (data) => {
 }
 
 const authenticate = async (email, password) => {
-    let res = await db.query(`SELECT * FROM ${table} WHERE (email=$1 OR phone=$2) AND password=$3 AND email_verified_at <= $4`, [email, email, password, getDate()]);
-    return res.rows[0] || null
+    let res = await db.query(`SELECT * FROM ${table} WHERE (email=$1 OR phone=$2) AND email_verified_at <= $3`, [email, email, getDate()]);
+    res = res.rows[0] || null
+    if(res.password && bcrypt.compareSync(password, res.password)){
+        return res;
+    }
+    return null;
 }
 
 const update = async (where, data) => {
+    data = {...data, updated_at: getDate()}
+    if(data.password){
+        data.password = bcrypt.hashSync(data.password, saltRounds);
+    }
     const whereKeys = Object.keys(where)
     const dataKeys = Object.keys(data)
     const set = dataKeys.map(key=>`${camelToSnakeCase(key)}='${data[key]}'`).join(', ')
