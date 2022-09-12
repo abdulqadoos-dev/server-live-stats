f995c912360ce802da076289f89416751a63d6d3
faisalsaeed-work <87143179+faisalsaeed-work@users.noreply.github.com>
2022-09-12 23:17:09 +0500

    Add validations in create games

--- source/app/controllers/GameController.js
+++ source/app/controllers/GameController.js
@@ -8,111 +8,114 @@ const modelInstance = require("../models");
 const GetAllGamesResponse = require("../responses/GetAllGamesResponse");
 const SportModal = modelInstance.sport;
 const TeamModal = modelInstance.team;
-const SuccessWithDataResponse = require('./../responses/SuccessWithDataResponse');
+const SuccessWithDataResponse = require("./../responses/SuccessWithDataResponse");
 const BadRequestResponse = require("../responses/BadRequestResponse");
 
-const create = async(req, res, next) => {
-    try {
-        const { sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround } = req.body;
-        if ((await validateIds({ sportId, mainTeamId, opponentTeamId }, res)))
-            return '';
-        if (await GameService.verifyScheduleTime({ dateTime, mainTeamId, opponentTeamId })) {
-            await GameService.create({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround })
-            return res.send(SuccessResponse('Game created successfully'))
-        }else{
-            return res.status(400).send(BadRequestResponse('Another game is scheduled at selected time!'));
-        }
-    } catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+const create = async (req, res, next) => {
+	try {
+		const { sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround } = req.body;
+        if(new Date(dateTime).getTime() < new Date().getTime())
+            return res.status(400).send(BadRequestResponse("Cannot create game for past date!"));
+		if (await validateIds({ sportId, mainTeamId, opponentTeamId }, res)) return "";
+		if (await GameService.verifyScheduleTime({ dateTime, mainTeamId, opponentTeamId })) {
+			await GameService.create({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround });
+			return res.send(SuccessResponse("Game created successfully"));
+		} else {
+			return res.status(400).send(BadRequestResponse("Another game is scheduled at selected time!"));
+		}
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
-const getAll = async(req, res, next) => {
-    try {
-        const { teamId } = req.params
-        const games = await GameService.getAll(teamId);
-        let gameDetails = [];
-        for (let game of games) {
-            game.mainTeam = await TeamService.getById(game.mainTeamId);
-            game.opponentTeam = await TeamService.getById(game.opponentTeamId);
-            game.sport = await SportService.getById(game.sportId);
-            gameDetails.push(game)
-        }
-        return res.send(GetAllGamesResponse(games))
-    } catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+const getAll = async (req, res, next) => {
+	try {
+		const { teamId } = req.params;
+		const games = await GameService.getAll(teamId);
+		let gameDetails = [];
+		for (let game of games) {
+			game.mainTeam = await TeamService.getById(game.mainTeamId);
+			game.opponentTeam = await TeamService.getById(game.opponentTeamId);
+			game.sport = await SportService.getById(game.sportId);
+			gameDetails.push(game);
+		}
+		return res.send(GetAllGamesResponse(games));
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
-const verifyScheduleTime = async(req, res, next) => {
-    try {
-        const { dateTime, mainTeamId, opponentTeamId } = req.body;
-        if (await GameService.verifyScheduleTime({ dateTime, mainTeamId, opponentTeamId })) {
-            return res.send(SuccessWithDataResponse({ isScheduled: true }))
-        }
-        return res.send(SuccessWithDataResponse({ isScheduled: false }))
-    } catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+const verifyScheduleTime = async (req, res, next) => {
+	try {
+		const { dateTime, mainTeamId, opponentTeamId } = req.body;
+		if (await GameService.verifyScheduleTime({ dateTime, mainTeamId, opponentTeamId })) {
+			return res.send(SuccessWithDataResponse({ isScheduled: true }));
+		}
+		return res.send(SuccessWithDataResponse({ isScheduled: false }));
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
 const getBySportId = async (req, res, next) => {
-    try{
-        const {sportId} = req.params;
-        const games = await GameService.getBySport(sportId)
-        return res.send(GetAllGamesResponse(games))
-    }catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+	try {
+		const { sportId } = req.params;
+		const games = await GameService.getBySport(sportId);
+		return res.send(GetAllGamesResponse(games));
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
-const validateIds = async({ sportId, mainTeamId, opponentTeamId }, res) => {
-    if (!(await SportModal.findByPk(sportId))?.id) {
-        return res.status(400).send(RequestValidationResponse({ sportId: 'Sport id is incorrect' }));
-    }
-    if (!(await TeamModal.findByPk(mainTeamId))?.id) {
-        return res.status(400).send(RequestValidationResponse({ mainTeamId: 'Main Team id is incorrect' }));
-    }
-    if (!(await TeamModal.findByPk(opponentTeamId))?.id) {
-        return res.status(400).send(RequestValidationResponse({ opponentTeamId: 'Opponent Team id is incorrect' }));
-    }
-    return false;
-}
+const validateIds = async ({ sportId, mainTeamId, opponentTeamId }, res) => {
+    if(mainTeamId == opponentTeamId)
+		return res.status(400).send(BadRequestResponse("Cannot create a match between two same teams!"));
+	if (!(await SportModal.findByPk(sportId))?.id) {
+		return res.status(400).send(RequestValidationResponse({ sportId: "Sport id is incorrect" }));
+	}
+	if (!(await TeamModal.findByPk(mainTeamId))?.id) {
+		return res.status(400).send(RequestValidationResponse({ mainTeamId: "Main Team id is incorrect" }));
+	}
+	if (!(await TeamModal.findByPk(opponentTeamId))?.id) {
+		return res.status(400).send(RequestValidationResponse({ opponentTeamId: "Opponent Team id is incorrect" }));
+	}
+	return false;
+};
 
 const update = async (req, res, next) => {
-    try{
-        const {id} = req.params;
-        if(!(await GameService.find(id))){
-            return res.status(400).send(RequestValidationResponse({ gameId: 'Game id is incorrect' }));
-        }
-        const {location, mainTeamPlayGround, opponentTeamPlayGround, dateTime, details} = req.body;
-        const game = await GameService.update(id, {location, mainTeamPlayGround, opponentTeamPlayGround, dateTime, details})
-        return res.send(SuccessWithDataResponse({game, message:'Game updated successfully'}));
-    }catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+	try {
+		const { id } = req.params;
+		if (!(await GameService.find(id))) {
+			return res.status(400).send(RequestValidationResponse({ gameId: "Game id is incorrect" }));
+		}
+		const { location, mainTeamPlayGround, opponentTeamPlayGround, dateTime, details } = req.body;
+		const game = await GameService.update(id, { location, mainTeamPlayGround, opponentTeamPlayGround, dateTime, details });
+		return res.send(SuccessWithDataResponse({ game, message: "Game updated successfully" }));
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
 const updateGameDetails = async (req, res) => {
-    try{
-        const {id} = req.params;
-        if(!(await GameService.find(id))){
-            return res.status(400).send(RequestValidationResponse({ gameId: 'Game id is incorrect' }));
-        }
-        const {details} = req.body;
-        const game = await GameService.update(id, {details})
-        IOGlobal.emit("broadcast_game_"+game.id, { game, time: new Date().getTime() });
-        return res.send(SuccessWithDataResponse({game, message:'Game details updated successfully'}));
-    }catch (err) {
-        return res.status(500).send(ExceptionResponse(err.message))
-    }
-}
+	try {
+		const { id } = req.params;
+		if (!(await GameService.find(id))) {
+			return res.status(400).send(RequestValidationResponse({ gameId: "Game id is incorrect" }));
+		}
+		const { details } = req.body;
+		const game = await GameService.update(id, { details });
+		IOGlobal.emit("broadcast_game_" + game.id, { game, time: new Date().getTime() });
+		return res.send(SuccessWithDataResponse({ game, message: "Game details updated successfully" }));
+	} catch (err) {
+		return res.status(500).send(ExceptionResponse(err.message));
+	}
+};
 
 module.exports = {
-    create,
-    getAll,
-    verifyScheduleTime,
-    getBySportId,
-    update,
-    updateGameDetails
-}
+	create,
+	getAll,
+	verifyScheduleTime,
+	getBySportId,
+	update,
+	updateGameDetails,
+};
--- source/app/services/GameService.js
+++ source/app/services/GameService.js
@@ -1,4 +1,4 @@
-const modelInstance = require('./../models/index')
+const modelInstance = require("./../models/index");
 const { addHoursToDate } = require("./HelperService");
 const GameModal = modelInstance.game;
 const TeamModal = modelInstance.team;
@@ -7,95 +7,121 @@ const MatchModel = modelInstance.match;
 const UserModel = modelInstance.user;
 const { Op } = require("sequelize");
 
-const create = async({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround }) => {
-    return await GameModal.create({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround })
-}
+const create = async ({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround }) => {
+	return await GameModal.create({ sportId, dateTime, location, mainTeamId, opponentTeamId, mainTeamPlayGround, opponentTeamPlayGround });
+};
 
-const getAll = async(teamId = null) => {
-    return teamId ? await GameModal.findAll({
-            where: {
-                [Op.or]: [{ mainTeamId: teamId }, { opponentTeamId: teamId }]
-            },
-            order: [
-                ['createdAt', 'DESC']
-            ],
-            raw: true
-        }) :
-        await GameModal.findAll({ raw: true });
-}
+const getAll = async (teamId = null) => {
+	return teamId
+		? await GameModal.findAll({
+				where: {
+					[Op.or]: [{ mainTeamId: teamId }, { opponentTeamId: teamId }],
+				},
+				order: [["createdAt", "DESC"]],
+				raw: true,
+		  })
+		: await GameModal.findAll({ raw: true });
+};
 
-const verifyScheduleTime = async({ dateTime = new Date(), mainTeamId, opponentTeamId }) => {
-    const newDateLesser = addHoursToDate(new Date(dateTime), -2)
-    const newDateGreater = addHoursToDate(new Date(dateTime), 2)
-    const gameTime = await GameModal.findAll({
-        where: {
-            [Op.and]: [
-                [{ mainTeamId }, {
-                    dateTime: {
-                        [Op.gte]: newDateLesser,
-                        [Op.lte]: newDateGreater
-                    }
-                }],
-                [{ opponentTeamId }, {
-                    dateTime: {
-                        [Op.gte]: newDateLesser,
-                        [Op.lte]: newDateGreater
-                    }
-                }]
-            ]
-        }
-    })
-    return gameTime.length === 0;
-}
+const verifyScheduleTime = async ({ dateTime = new Date(), mainTeamId, opponentTeamId }) => {
+	const newDateLesser = addHoursToDate(new Date(dateTime), -2);
+	const newDateGreater = addHoursToDate(new Date(dateTime), 2);
+	const gameTime = await GameModal.findAll({
+		where: {
+			[Op.or]: [
+				{
+					[Op.and]: [
+						[
+							{ mainTeamId },
+							{
+								dateTime: {
+									[Op.gte]: newDateLesser,
+									[Op.lte]: newDateGreater,
+								},
+							},
+						],
+						[
+							{ opponentTeamId },
+							{
+								dateTime: {
+									[Op.gte]: newDateLesser,
+									[Op.lte]: newDateGreater,
+								},
+							},
+						],
+					],
+				},
+				{
+					[Op.and]: [
+						[
+							{ mainTeamId: opponentTeamId },
+							{
+								dateTime: {
+									[Op.gte]: newDateLesser,
+									[Op.lte]: newDateGreater,
+								},
+							},
+						],
+						[
+							{ opponentTeamId: mainTeamId },
+							{
+								dateTime: {
+									[Op.gte]: newDateLesser,
+									[Op.lte]: newDateGreater,
+								},
+							},
+						],
+					],
+				},
+			],
+		},
+	});
+	return gameTime.length === 0;
+};
 
-const verifyId = async(id) => {
-    return Boolean(await GameModal.findByPk(id))
-}
+const verifyId = async (id) => {
+	return Boolean(await GameModal.findByPk(id));
+};
 
 const getByMainTeamId = async (mainTeamId) => {
-    return await GameModal.findAll({where:{mainTeamId}});
-}
+	return await GameModal.findAll({ where: { mainTeamId } });
+};
 
 const getBySport = async (sportId) => {
-    return await GameModal.findAll({where:{sportId}, include:[
-        {model: TeamModal, as: 'mainTeam', foreignKey: 'mainTeamId', 
-            include:{model: UserModel, as: "user", foreignKey: "userId", attributes:['id', 'image']}
-        },
-        {model: TeamModal, as: 'opponentTeam', foreignKey: 'opponentTeamId',
-            include:{model: UserModel, as: "user", foreignKey: "userId", attributes:['id', 'image']}
-        },
-        {model: SportModal, as: 'sport', foreignKey: 'sportId'}
-    ]})
-}
+	return await GameModal.findAll({
+		where: { sportId },
+		include: [
+			{ model: TeamModal, as: "mainTeam", foreignKey: "mainTeamId", include: { model: UserModel, as: "user", foreignKey: "userId", attributes: ["id", "image"] } },
+			{ model: TeamModal, as: "opponentTeam", foreignKey: "opponentTeamId", include: { model: UserModel, as: "user", foreignKey: "userId", attributes: ["id", "image"] } },
+			{ model: SportModal, as: "sport", foreignKey: "sportId" },
+		],
+	});
+};
 
 const find = async (id) => {
-    return await GameModal.findOne({
-        where: { id },
-        include: [
-            { model: TeamModal, as: "mainTeam", foreignKey: "mainTeamId", 
-                include:{model: UserModel, as: "user", foreignKey: "userId", attributes:['id', 'image']}
-            },
-            { model: TeamModal, as: "opponentTeam", foreignKey: "opponentTeamId", 
-                include:{model: UserModel, as: "user", foreignKey: "userId", attributes:['id', 'image']}
-            },
-            { model: SportModal, as: "sport", foreignKey: "sportId" },
-            // { model: MatchModel, as: "match", foreignKey:"gameId"}
-        ],
-    });
-}
+	return await GameModal.findOne({
+		where: { id },
+		include: [
+			{ model: TeamModal, as: "mainTeam", foreignKey: "mainTeamId", include: { model: UserModel, as: "user", foreignKey: "userId", attributes: ["id", "image"] } },
+			{ model: TeamModal, as: "opponentTeam", foreignKey: "opponentTeamId", include: { model: UserModel, as: "user", foreignKey: "userId", attributes: ["id", "image"] } },
+			{ model: SportModal, as: "sport", foreignKey: "sportId" },
+			// { model: MatchModel, as: "match", foreignKey:"gameId"}
+		],
+	});
+};
 
 const update = async (id, requestBody) => {
-    await GameModal.update(requestBody, {where:{id}})
-    return await find(id)
-}
+	await GameModal.update(requestBody, { where: { id } });
+	return await find(id);
+};
 
 module.exports = {
-    create,
-    getAll,
-    verifyScheduleTime,
-    verifyId,
-    getByMainTeamId,
-    getBySport,
-    find,
-    update
-}
+	create,
+	getAll,
+	verifyScheduleTime,
+	verifyId,
+	getByMainTeamId,
+	getBySport,
+	find,
+	update,
+};
